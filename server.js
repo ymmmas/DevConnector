60d209cd4d2f257f826e19ccf78e60252e0607c9
Sammy Liu <xliu95@buffalo.edu>
2022-06-27 21:02:19 -0400

    connected mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // express server
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// connect database
+connectDB();
 
 app.get('/', (req,res)=> res.send('API Running'));
 

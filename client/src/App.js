661bcf12222c01d67507e542cfa8beb0a69348e8
Sammy Liu <xliu95@buffalo.edu>
2022-07-01 22:52:25 -0400

    started setting up react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, {Fragment} from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+
+const  App = () =>
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
+
+
+export default App;
